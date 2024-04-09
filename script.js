b99079ab65d2c90415d5396fb8c62dc3e3347ee5
FraQuartu <francescoquartu48@gmail.com>
2024-04-09 23:14:22 +0200

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+  const randomNumber = Math.ceil(Math.random() * 3);
+  if(randomNumber == 1){
+    return "Rock";
+  }
+  else if(randomNumber == 2){
+    return "Paper";
+  }
+  else {
+    return "Scissors";
+  }
+}
