0faff08a05af880dcf50c8524214c23797a94449
FraQuartu <francescoquartu48@gmail.com>
2024-04-09 23:35:05 +0200

    Add playGame function

--- script.js
+++ script.js
@@ -17,21 +17,63 @@ function getUserChoice() {
 
 function playRound(userChoice, computerChoice) {
   if(userChoice == computerChoice) {
-    return "Tie!";
+    return "Tie";
   }
   else if((userChoice == "Rock" && computerChoice == "Scissors") ||
     (userChoice == "Paper" && computerChoice == "Rock") ||
     (userChoice == "Scissors" && computerChoice == "Paper")) {
     
-    return "You win! " + userChoice + " beats " + computerChoice + "!";
+    return "User";
   }
   else if((userChoice == "Rock" && computerChoice == "Paper") ||
     (userChoice == "Paper" && computerChoice == "Scissors") ||
     (userChoice == "Scissors" && computerChoice == "Rock")) {
     
-    return "You lost! " + userChoice + " loses against " + computerChoice + "!";
+    return "Computer";
   }
   else {
-    return "Invalid input!";
+    return "Error";
   }
+}
+
+function playGame() {
+  let userScore = 0;
+  let computerScore = 0;
+
+  let winner = playRound(getUserChoice(), getComputerChoice());
+  console.log(winner);
+  if(winner == "User") {userScore+=1;}
+  else if(winner == "Computer") {computerScore+=1;}
+
+  winner = playRound(getUserChoice(), getComputerChoice());
+  console.log(winner);
+  if(winner == "User") {userScore+=1;}
+  else if(winner == "Computer") {computerScore+=1;}
+
+  winner = playRound(getUserChoice(), getComputerChoice());
+  console.log(winner);
+  if(winner == "User") {userScore+=1;}
+  else if(winner == "Computer") {computerScore+=1;}
+
+  winner = playRound(getUserChoice(), getComputerChoice());
+  console.log(winner);
+  if(winner == "User") {userScore+=1;}
+  else if(winner == "Computer") {computerScore+=1;}
+
+  winner = playRound(getUserChoice(), getComputerChoice());
+  console.log(winner);
+  if(winner == "User") {userScore+=1;}
+  else if(winner == "Computer") {computerScore+=1;}
+
+
+  if(userScore > computerScore) {
+    console.log("You win!");
+  }
+  else if(userScore < computerScore) {
+    console.log("You lose!");
+  }
+  else {
+    console.log("Tie!");
+  }
+
 }
