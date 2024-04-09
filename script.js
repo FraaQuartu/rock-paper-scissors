3f5837013d390c9764ac2eae9ea7056014bf6581
FraQuartu <francescoquartu48@gmail.com>
2024-04-09 23:25:49 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
+
+function getUserChoice() {
+  return prompt("Choose Rock, Paper or Scissors");
+}
+
+function playRound(userChoice, computerChoice) {
+  if(userChoice == computerChoice) {
+    return "Tie!";
+  }
+  else if((userChoice == "Rock" && computerChoice == "Scissors") ||
+    (userChoice == "Paper" && computerChoice == "Rock") ||
+    (userChoice == "Scissors" && computerChoice == "Paper")) {
+    
+    return "You win! " + userChoice + " beats " + computerChoice + "!";
+  }
+  else if((userChoice == "Rock" && computerChoice == "Paper") ||
+    (userChoice == "Paper" && computerChoice == "Scissors") ||
+    (userChoice == "Scissors" && computerChoice == "Rock")) {
+    
+    return "You lost! " + userChoice + " loses against " + computerChoice + "!";
+  }
+  else {
+    return "Invalid input!";
+  }
+}
